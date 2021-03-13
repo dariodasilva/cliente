47521aaae7577c146bca9e5cad9ab2085db3c18c
dariodasilva <dariotecinfo@gmail.com>
2021-03-13 18:52:40 -0300

    Injeção de dependências e insert no banco

--- src/main/java/io/github/dariodasilva/clientes/ClientesApplication.java
+++ src/main/java/io/github/dariodasilva/clientes/ClientesApplication.java
@@ -1,10 +1,25 @@
 package io.github.dariodasilva.clientes;
 
+import io.github.dariodasilva.clientes.model.entity.Cliente;
+import io.github.dariodasilva.clientes.model.entity.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+    @Bean
+    public CommandLineRunner run (@Autowired ClienteRepository repository) {
+        return args -> {
+            Cliente cliente = new Cliente();
+            cliente.setNome("Dario");
+            cliente.setCpf("00984374475");
+            repository.save(cliente);
+        };
+    }
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
 
--- src/main/java/io/github/dariodasilva/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/dariodasilva/clientes/model/entity/Cliente.java
@@ -1,12 +1,16 @@
 package io.github.dariodasilva.clientes.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
     @Id
