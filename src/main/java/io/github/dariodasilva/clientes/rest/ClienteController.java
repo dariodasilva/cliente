1d3e0ee06a406a353ff0e051ee71398d65ad8035
dariodasilva <dariotecinfo@gmail.com>
2021-03-13 20:43:41 -0300

    Recuperando dados do usuário

---
+++ src/main/java/io/github/dariodasilva/clientes/rest/ClienteController.java
@@ -0,0 +1,35 @@
+package io.github.dariodasilva.clientes.rest;
+
+import io.github.dariodasilva.clientes.model.entity.Cliente;
+import io.github.dariodasilva.clientes.model.entity.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.swing.*;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    public final ClienteRepository repository;
+
+    @Autowired
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar( @RequestBody Cliente cliente){
+        return repository.save(cliente);
+
+    }
+
+    @GetMapping("{id}")
+    public Cliente obterPorId(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+}
