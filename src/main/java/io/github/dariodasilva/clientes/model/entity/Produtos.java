128dac993bc99be5d708c7ae082ba3eb7f4de3e3
dariodasilva <dariotecinfo@gmail.com>
2021-03-15 11:09:42 -0300

    Adicionando rota delete para o Cliente

---
+++ src/main/java/io/github/dariodasilva/clientes/model/entity/Produtos.java
@@ -0,0 +1,32 @@
+package io.github.dariodasilva.clientes.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Produtos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column( name = "nome_produto",nullable = false, length = 200)
+    private String nomeProduto;
+
+    @Column( nullable = false, length = 150)
+    private String descricao;
+
+    @Column( nullable = false, length = 150)
+    private String marca;
+
+
+
+}
